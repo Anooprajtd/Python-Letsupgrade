32f51b87543ed90a6a841dc9b7fe60ae5c6555b0
Anooprajtd <36911358+Anooprajtd@users.noreply.github.com>
2020-07-21 20:46:03 +0530

    Add files via upload

---
+++ Prog-1.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+print("Hi")
+
+
+# In[2]:
+
+
+print("Welcome to Java")
+
+
+# This is a markdown") and this is just to take down notes
+# Well lets see what it is
+this is a raw notebook
+print("This is a raw notebook")
+x = 4
+if x > 5
+print ("X is smaller")
+# ## HI this is headding
+# 
+
+# In[11]:
+
+
+print (" hello anoop")
+
+
+# In[26]:
+
+
+name = "Anoop"
+marks= 90.2000
+age = 34
+print ("Name: %s\nMarks: %.2f \nage: %d" %(name,marks,age))
+
+
+# In[30]:
+
+
+print (f"Name: {name}\nMarks: {marks} \nage: {age}" )
+
+
+# In[31]:
+
+
+mark = 10
+id(mark)
+
+
+# In[32]:
+
+
+name = "Anoop"
+id(name)
+
+
+# In[33]:
+
+
+10-20
+
+
+# In[34]:
+
+
+10/3
+
+
+# In[35]:
+
+
+10%3
+
+
+# In[36]:
+
+
+4/2
+
+
+# In[37]:
+
+
+4%2
+
+
+# In[38]:
+
+
+10//3
+
+
+# In[45]:
+
+
+a = 3
+b = a
+a == b
+
+
+# In[42]:
+
+
+a < b
+
+
+# In[46]:
+
+
+a == b
+
+
+# In[47]:
+
+
+a != b
+
+
+# In[48]:
+
+
+a | b
+
+
+# In[49]:
+
+
+bin(a)
+
+
+# In[50]:
+
+
+hex(a)
+
+
+# In[51]:
+
+
+a = 240 
+b = 1
+
+
+# In[52]:
+
+
+a | b
+
+
+# In[53]:
+
+
+bin (a)
+
+
+# In[54]:
+
+
+20 > M
+
+
+# In[58]:
+
+
+str = "welcome to the programming language"
+"A" not in str
+
+
+# 2**
+
+# In[59]:
+
+
+2**2
+
+
+# In[60]:
+
+
+2**5
+
+
+# In[ ]:
+
+
+
+
